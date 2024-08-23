b4473c2f6f9fd17f3972511d258340e652c49168
ashishkunthe <ashishkunthe2003@gmail.com>
2024-08-23 21:24:08 +0530

    dom

--- src/App.js
+++ src/App.js
@@ -202,9 +202,20 @@ function Logo() {
 function Search({ setQuery, query }) {
   const inputEl = useRef(null);
 
-  useEffect(function () {
-    inputEl.current.focus();
-  }, []);
+  useEffect(
+    function () {
+      function callback(e) {
+        if (document.activeElement === inputEl.current) return;
+        if (e.code === "Enter") {
+          inputEl.current.focus();
+          setQuery("");
+        }
+      }
+      document.addEventListener("keydown", callback);
+      return () => document.addEventListener("keydown", callback);
+    },
+    [setQuery]
+  );
   return (
     <input
       className="search"
